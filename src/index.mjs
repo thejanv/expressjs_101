caea06f7e5d6808549a395e1dc2c806352039554
Thejan Vithanage <thejanvithanage@outlook.com>
2024-06-27 21:30:56 +0530

    Cookies

--- src/index.mjs
+++ src/index.mjs
@@ -1,10 +1,16 @@
 import express from "express";
 import routes from "./routes/index.mjs";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser("helloworld"));
 app.use("/api", routes);
+
+app.get("/", (req, res) => {
+  res.cookie("hello", "world", { maxAge: 10000, signed: true }).sendStatus(200);
+});
 
 const port = process.env.PORT || 3000;
 
--- src/routes/products.mjs
+++ src/routes/products.mjs
@@ -3,18 +3,21 @@ import { Router } from "express";
 const router = Router();
 
 router.get("", (req, res) => {
-  res.send([
-    {
-      id: 1001,
-      username: "chicken",
-      displayname: "Chicken",
-    },
-    {
-      id: 1002,
-      username: "pork",
-      displayname: "Pork",
-    },
-  ]);
+  if (req.signedCookies && req.signedCookies.hello === "world") {
+    return res.send([
+      {
+        id: 1001,
+        username: "chicken",
+        displayname: "Chicken",
+      },
+      {
+        id: 1002,
+        username: "pork",
+        displayname: "Pork",
+      },
+    ]);
+  }
+  return res.sendStatus(401);
 });
 
 export default router;
