4f6eb6388c5b174c54131a5505be4523fdd66f5d
Thejan Vithanage <thejanvithanage@outlook.com>
2024-06-27 11:30:41 +0530

    DELETE Requests

--- src/index.mjs
+++ src/index.mjs
@@ -109,6 +109,17 @@ app.patch("/api/users/id", (req, res) => {
   return res.sendStatus(200);
 });
 
+// Delete a user
+app.delete("/api/users/:id", (req, res) => {
+  const { id } = req.params;
+  const parseID = parseInt(id);
+  if (isNaN(parseID)) return res.sendStatus(400);
+  const findUserIndex = mockUsers.findIndex((user) => user.id === parseID);
+  if (findUserIndex === -1) return res.sendStatus(404);
+  mockUsers.splice(findUserIndex, 1);
+  return res.sendStatus(200);
+});
+
 app.get("/api/products", (req, res) => {
   res.send([
     {
